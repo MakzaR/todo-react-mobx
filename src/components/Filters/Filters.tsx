a63f590adcc3ad5c7564adaa0f38d83cc58d6e94
MakzaR <makzarchukov@gmail.com>
2022-09-15 17:28:08 +0500

    added styles for filters

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import {observer} from "mobx-react-lite";
 import todoStore from "../../store/todoStore";
 
+import styles from './Filter.module.scss';
+
 const Filters = observer(() => {
+    const currentFilter = todoStore.filter;
+
+    function getButtonStyles(filter: string) {
+        return `${styles.filterButton}  ${currentFilter === filter ? styles.activeButton : ''}`;
+    }
+
     return (
-        <div>
-            <button onClick={() => todoStore.changeFilter('all')}>Все</button>
-            <button onClick={() => todoStore.changeFilter('completed')}>Выполненные</button>
-            <button onClick={() => todoStore.changeFilter('open')}>Невыполненные</button>
+        <div className={styles.filtersContainer}>
+            <button className={getButtonStyles('all')} onClick={() => todoStore.changeFilter('all')}>
+                Все
+            </button>
+            <button className={getButtonStyles('completed')} onClick={() => todoStore.changeFilter('completed')}>
+                Выполненные
+            </button>
+            <button className={getButtonStyles('open')} onClick={() => todoStore.changeFilter('open')}>
+                Невыполненные
+            </button>
         </div>
     );
 })
